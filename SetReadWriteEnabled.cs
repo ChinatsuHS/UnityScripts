dbe4b48ddd12203ff942fa4c506c582612cd26bd
ChinatsuHS <ChinatsuHS@users.noreply.github.com>
2023-09-18 05:11:31 +0200

    Initial add of files
    Added AlphaTextureDetector and SetReadWriteEnbled

---
+++ SetReadWriteEnabled.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SetReadWriteEnabled : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.isReadable = true;
+    }
+}
