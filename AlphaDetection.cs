60c181d7d772c1b54ead4ee572a70bce96f30d5e
ChinatsuHS <ChinatsuHS@users.noreply.github.com>
2023-09-18 06:25:25 +0200

    Update AlphaDetection.cs

--- AlphaDetection.cs
+++ AlphaDetection.cs
@@ -1,14 +1,15 @@
 ﻿#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class ImageAlphaDetection : AssetPostprocessor
 {
     // This method is called by Unity whenever an asset is imported
-    private void OnPostprocessTexture(Texture2D texture)
+    private async void OnPostprocessTexture(Texture2D texture)
     {
-        // Check if the imported texture has alpha transparency
-        bool hasAlpha = TextureHasAlpha(texture);
+        // Check if the imported texture has alpha transparency asynchronously
+        bool hasAlpha = await Task.Run(() => TextureHasAlpha(texture));
 
         // Set the "Alpha Is Transparent" property based on the alpha transparency
         TextureImporter textureImporter = assetImporter as TextureImporter;
@@ -17,9 +18,9 @@ public class ImageAlphaDetection : AssetPostprocessor
             textureImporter.alphaIsTransparency = hasAlpha;
             textureImporter.isReadable = true;
             textureImporter.streamingMipmaps = true;
-            
+
             Debug.Log($"Alpha Is Transparent set to {hasAlpha}, Read/Write Enabled set to true, and Streaming MipMaps set to true for texture: {textureImporter.assetPath}");
-            
+
             AssetDatabase.ImportAsset(textureImporter.assetPath);
         }
     }
